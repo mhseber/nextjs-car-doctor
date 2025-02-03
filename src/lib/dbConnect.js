6b80c25ae2011d4f54b7f5dfbd7c2cb04a0ca578
mahamudul hasan seber <sebermh@gmail.com>
2025-02-04 01:00:50 +0600

    ServiesSection Done

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,18 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ox336.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+
+export default function dbConnect(collectionName) {
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    return client.db().collection(collectionName);
+};
+
+
